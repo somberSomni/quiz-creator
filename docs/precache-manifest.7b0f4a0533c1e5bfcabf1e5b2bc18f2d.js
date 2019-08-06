2bf4d02e2950d98fce9b83de79fdd52bd9935368
somberSomni <kartunehustla@gmail.com>
2019-08-06 03:20:10 -0500

    publish github project. Need more updates

---
+++ docs/precache-manifest.7b0f4a0533c1e5bfcabf1e5b2bc18f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219f5f801bdf8c39bcbb1b2d4aa6413e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e5ee818cb93dc130d12",
+    "url": "/static/css/main.9a1d71ff.chunk.css"
+  },
+  {
+    "revision": "64574c5143492a83f482",
+    "url": "/static/js/2.1ff96fca.chunk.js"
+  },
+  {
+    "revision": "8e5ee818cb93dc130d12",
+    "url": "/static/js/main.f6632c6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
